9b3832264119c4d5c196e305df8585c3575e2931
Naoyuki Arakawa <56021399+NaoyukiArakawa@users.noreply.github.com>
2020-10-26 15:38:53 +0100

    styling up

--- src/app.js
+++ src/app.js
@@ -28,15 +28,14 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    title: 'About Me',
+    title: 'About This Project',
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
-    name: 'Naoyuki',
-    text: 'This is our help. Below you can find each help articles.',
+    text: 'This is our help page. Below you can find each help articles...',
   });
 });
 
